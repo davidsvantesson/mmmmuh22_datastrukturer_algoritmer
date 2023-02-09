dc228ef9b803965f6afe2b3387e56a8f5ae9cd3d
davidsvantesson <davidsvantesson@gmail.com>
2023-02-09 10:38:08 +0100

    Add skeleton for cpp of sum_list

---
+++ Lectures/Lecture 6/sum_list.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+int sum_list(vector<int> myVector) {
+    // Implementera
+    return 0;
+}
+
+int main() {
+    vector<int> myVector = {5, 4, 2, 7, 1};
+    cout << "Sum of list: " << sum_list(myVector) << endl;
+}
